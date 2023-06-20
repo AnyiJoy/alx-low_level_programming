f9caf3b5c91320160463612c0f03ff3c7478c9aa
AnyiJoy <anyijoy097@gmail.com>
2023-06-20 16:16:27 -0700

    C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,18 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase
  */
-void print_alphabet(void{
+void print_alphabet(void) {
     char letter = 'a';
 
-    while (letter <= 'z')
-    {
+    while (letter <= 'z') {
         _putchar(letter);
         letter++;
     }
 
     _putchar('\n');
+}
+
+int main(void) {
+    print_alphabet();
+    return 0;
 }

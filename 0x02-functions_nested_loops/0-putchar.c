bbbdb6aa9c52011c9028e0f12127c4e2f34056ff
AnyiJoy <anyijoy097@gmail.com>
2023-06-20 15:45:50 -0700

    C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point of the program.
  *
- * Description: Prints _ putchar, followed by a newline character.
+ * Description: Prints _putchar, followed by a newline character.
  *
  * Return: 0 if successful, non-zero otherwise.
  */
 int main(void)
 {
-	char str[] = "_ putchar";
+	char str[] = "_putchar";
 	int i;
 
 	for (i = 0; str[i] != '\0'; ++i)
@@ -23,13 +23,13 @@ int main(void)
 }
 
 /**
- * _ putchar - Print a character to stdout.
+ * _putchar - Print a character to stdout.
  *
  * @c: The character to be printed.
  *
  * Return: On success, return 1. Otherwise, return -1 on error with errno set appropriately.
  */
-int _ putchar(char c)
+int _putchar(char c)
 {
 	return write(1 , &c , 1 );
 }

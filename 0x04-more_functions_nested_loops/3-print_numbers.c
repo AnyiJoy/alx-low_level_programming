861f587b2a4caa117800a5ecd40f152b303e8005
AnyiJoy <anyijoy097@gmail.com>
2023-06-22 17:06:23 -0700

    C Functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- * print_numbers - function that checks for uppercase character.
- * Return: 0
+ * print_numbers - print the numbers from 0 to 9
+ * Description: You can only use _putchar twice
  */
 void print_numbers(void)
 {
-	char c = '0';
+	int i;
 
-	while (c <= '9')
+	i = 0;
+	while (i < 10)
 	{
-		_putchar(c);
-		c++;
+		_putchar(i + '0');
+		i++;
 	}
-_putchar('\n');
+	_putchar('\n');
 }

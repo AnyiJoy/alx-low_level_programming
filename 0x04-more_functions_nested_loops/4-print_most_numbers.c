868bc67d70cecfb52319b3cb6113e5db3a8cd2b8
AnyiJoy <anyijoy097@gmail.com>
2023-06-25 09:42:38 -0700

    done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
- * print_most_numbers - Print numbers 0 to 9, excluding 2 and 4.
- * Description: Can only use _putchar twice.
+ * print_most_numbers - print 0 to 9
+ * except 2 and 4
  */
 void print_most_numbers(void)
 {
-	int i;
-
-	i = 0;
-	while (i < 10)
+	int num;	
+	
+	for (num = 0; num <= 9; num++)
 	{
-		if (i != 2 && i != 4)
-			_putchar(i + '0');
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
 
-		i++;
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,4 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_triangle(int size);
 
-#endif /* MAIN_H */
+#endif

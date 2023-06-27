4f83260e31890d3647ed8022b0badc8756fcc2f9
AnyiJoy <anyijoy097@gmail.com>
2023-06-27 13:32:54 -0700

    Password cracked

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,43 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
-
-#define PASSWORD_LENGTH 8
 
 /**
- * generate_password - generates a random password
- * Return: a pointer to the generated password
- */
-char *generate_password(void)
-{
-char *password = malloc(PASSWORD_LENGTH + 1);
-char chars_upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char chars_lower[] = "abcdefghijklmnopqrstuvwxyz";
-char chars_digits[] = "0123456789";
-char *chars = strcat(strcat(chars_upper, chars_lower), chars_digits);
-
-srand(time(NULL));
-
-for (int i = 0; i < PASSWORD_LENGTH; i++)
-{
-password[i] = chars[rand() % (sizeof(chars) - 1)];
-}
-
-password[PASSWORD_LENGTH] = '\0';
-
-return (password);
-}
-
-/**
- * main - main function
- * Return: 0 on success
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-char *password = generate_password();
-printf("Generated password: %s\n", password);
-free(password);
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-return (0);
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+
+	return (0);
 }

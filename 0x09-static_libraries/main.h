f7556f91036efad926d0b9b4885f46f808dc7a9e
AnyiJoy <anyijoy097@gmail.com>
2023-07-07 13:47:06 -0700

    DONE

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+#include <stddef.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int main(void);
-#endif
+
+#endif /* MAIN_H */
